72bf07cd1f5cb3c47afb9dab829426e345be7b28
sumukshashidhar <sumukuuu@gmail.com>
2020-08-24 23:45:58 +0530

    :bug: files must be sorted after removing log days

--- roam_backup_system/index_creator.py
+++ roam_backup_system/index_creator.py
@@ -30,10 +30,10 @@ def create_index():
     """
     global DOCS_PATH
     files = enum(DOCS_PATH)
-    files.sort()
     logs = identify_log_days(files)
     for i in logs:
         files.remove(i)
+    files.sort()
     with open("./../docs/"+"index.md", 'w+') as f:
         f.write("# List of All Files.\n")
         f.write("## Pages\n")
