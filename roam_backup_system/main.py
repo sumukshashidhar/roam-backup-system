e59e17f2b64751c44925fdb8cb1656975ef2c82d
sumukshashidhar <sumukuuu@gmail.com>
2020-08-24 22:42:28 +0530

    :sparkles: added argument parsing. fixes #5

--- roam_backup_system/main.py
+++ roam_backup_system/main.py
@@ -1,10 +1,8 @@
 # CONSTANTS: MODIFY FOR YOUR NEEDS
 
-
+import argparse
 import git_push as gitpusher
 from enumeration import enum
-from remove_whitespace import rem_whitespaces as rw
-from remove_whitespace import remove_question_marks as rq
 import fs
 from backlinks import add_backlink as add_backlinks
 from index_creator import create_index as create_index
@@ -14,10 +12,12 @@ GITPUSH = True
 
 
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--push", help="Push to Github")
+	args = parser.parse_args()
+	GITPUSH = args.push if args.push is not None else True
 	# let us run the whitespace remover first, to ensure everything
 	# has an underscore - GH pages does not recognize links without it
-	# rw() # appears as though whitespaces are being handled well. No need of this random stuff
-	rq()
 	# first enumerate the directories
 	files = enum()
 	# make the backup dir if it doesn't exist at first
