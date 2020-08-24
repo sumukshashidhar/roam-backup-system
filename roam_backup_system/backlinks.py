2d41d9843995cee21bb11588fd8dce8c1d7e9603
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-08-24 15:00:54 +0530

    :bug: The backlinks wouldn't render on GH pages.

--- roam_backup_system/backlinks.py
+++ roam_backup_system/backlinks.py
@@ -9,7 +9,7 @@ def add_backlink():
     for i in reference_map.keys():
         # TODO: HAS TO BE CHANGED IN THE FUTURE. VERY MESSY FILEPATH
         with open("./../docs/"+i+".md", 'a') as f:
-            f.write('\n \n # Backlinks  ')
+            f.write('\n \n# Backlinks')
             f.write('\n \n')
             for j in reference_map[i]:
                 f.write(f'- [{j[:-3]}]({j}) \n')
